b926c93d67f47ad05b9500f9d3166dc4585848e6
wangkaibo12138 <147256355+wangkaibo12138@users.noreply.github.com>
2025-09-16 21:59:28 +0800

    Add files via upload

---
+++ flow.py
@@ -0,0 +1,36 @@
+import normflows as nf
+import torch
+import torch.nn as nn
+from normflows.flows import Planar, Radial
+
+class Flow(nn.Module):
+    def __init__(self, latent_size, flow, K):
+        super().__init__()
+        if flow == "Planar":
+            flows = [Planar((latent_size,)) for _ in range(K)]
+        elif flow == "Radial":
+            flows = [Radial((latent_size,)) for _ in range(K)]
+        elif flow == "RealNVP":
+            flows = []
+            b = torch.Tensor([1 if i % 2 == 0 else 0 for i in range(latent_size)])
+            for i in range(K):
+                s = nf.nets.MLP([latent_size, 2 * latent_size, latent_size], init_zeros=True)
+                t = nf.nets.MLP([latent_size, 2 * latent_size, latent_size], init_zeros=True)
+                if i % 2 == 0:
+                    flows += [nf.flows.MaskedAffineFlow(b, t, s)]
+                else:
+                    flows += [nf.flows.MaskedAffineFlow(1 - b, t, s)]
+        self.flows = nn.ModuleList(flows)
+
+    def forward(self, z, base_dist, prior=None):
+        ld = 0.0
+        p_0 = torch.sum(base_dist.log_prob(z), -1)
+        for flow in self.flows:
+            z.requires_grad_(True)  # 确保 z 需要梯度
+            z, ld_ = flow(z)
+            ld += ld_
+        if prior:
+            kld = p_0 - torch.sum(prior.log_prob(z), -1) - ld.view(-1)
+        else:
+            kld = torch.zeros_like(p_0)  # Return zeros if no prior is provided
+        return z, kld
